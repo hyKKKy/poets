2bba212d0523ebbfe7933621a4ea1cfbf5ee82d9
Dmytro <dberezhkov2016@gmail.com>
2025-06-27 00:00:30 +0300

    Done

--- src/Components/Components.js
+++ src/Components/Components.js
@@ -1,15 +1,58 @@
 export function Shevchenko(){
     return(
         <div>
-            <p>Lorem ipsum</p>
+            <div className="header">
+                <img className="poetPic" src="/images/Taras_Shevchenko_-_portrait_by_Ivan_Kramskoi.jpg"></img>
+                <div className="title">
+                <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
+                <p>Hasellus sit amet quam id sapien malesuada dignissim nec feugiat enim. Nunc nunc enim, dictum sed ipsum ac, luctus vulputate leo. Phasellus convallis aliquet erat, eu pellentesque metus. Etiam vel molestie magna, ut porta lorem. Donec cursus elit nec maximus mattis. Cras consectetur mattis lobortis. Sed quis urna et orci vehicula volutpat. Donec pulvinar felis vel finibus lacinia. Sed sed ornare ante. 
+                    Nulla nec metus id est ullamcorper malesuada. Nullam magna nibh, lobortis sit amet porttitor eget, dictum finibus lorem. Morbi congue auctor elit, at ultricies velit varius ut. Suspendisse gravida, quam sit amet tincidunt dapibus, nunc nisi viverra velit, quis ultricies dui orci non lectus. Suspendisse non lobortis tortor. Ut id turpis ac risus venenatis elementum. Vivamus vel semper libero.
+
+                 </p></div>
+            </div>
+            <div className="general">
+                <p>
+                Phasellus sit amet quam id sapien malesuada dignissim nec feugiat enim. Nunc nunc enim, dictum sed ipsum ac, luctus vulputate leo. Phasellus convallis aliquet erat, eu pellentesque metus. Etiam vel molestie magna, ut porta lorem. Donec cursus elit nec maximus mattis. Cras consectetur mattis lobortis. Sed quis urna et orci vehicula volutpat. Donec pulvinar felis vel finibus lacinia. Sed sed ornare ante. Nulla nec metus id est ullamcorper malesuada. Nullam magna nibh, lobortis sit amet porttitor eget, dictum finibus lorem. Morbi congue auctor elit, at ultricies velit varius ut. Suspendisse gravida, quam sit amet tincidunt dapibus, nunc nisi viverra velit, quis ultricies dui orci non lectus. Suspendisse non lobortis tortor. Ut id turpis ac risus venenatis elementum. Vivamus vel semper libero.
+
+                Nam non nisi non nulla ornare feugiat quis ac nibh. Integer in efficitur nunc, vel sodales tortor. Sed pharetra mauris nisi, vitae semper felis vehicula at. Maecenas in lacus a dui congue convallis. Mauris tristique mollis tellus et efficitur. Duis consequat mi nisl, et pellentesque orci aliquam id. Vestibulum quis feugiat nisi. Mauris volutpat ultrices feugiat. Fusce placerat mi quis pretium eleifend. Morbi facilisis libero nec ornare aliquam. Suspendisse laoreet aliquam posuere. Fusce bibendum scelerisque quam, vitae congue quam tincidunt at. Nullam a neque nec felis maximus congue eu non arcu. Suspendisse euismod vel ipsum lobortis laoreet. Donec pharetra tristique eleifend.
+
+                Suspendisse potenti. Maecenas sit amet justo libero. Nullam viverra nulla ut ligula placerat ornare. Pellentesque ut ipsum odio. Donec nisl lectus, sodales quis maximus ut, rhoncus quis libero. Aliquam quis lectus quis massa lobortis aliquam. Pellentesque non justo mi. Nullam nec lacus tellus. Fusce diam quam, luctus sed ultrices a, blandit quis metus. Sed gravida vestibulum lorem, quis lacinia eros dictum in. Nullam ac tincidunt nibh. Vivamus pharetra ipsum vitae magna vestibulum, at lacinia velit aliquam.
+
+                Vivamus quis nisl velit. Pellentesque fermentum, libero ac interdum dapibus, urna eros consectetur felis, vel efficitur lacus diam imperdiet elit. Mauris consectetur sapien urna, eleifend commodo lacus elementum id. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc dictum rhoncus nisl, vitae malesuada dui maximus id. Morbi varius dignissim est sed pharetra. Aenean quis purus nec enim hendrerit convallis at ornare sapien. In ultricies eros non nulla auctor, vel viverra turpis aliquam.
+
+                Pellentesque tempus placerat ex, ac tristique velit. Cras hendrerit suscipit ipsum, et tincidunt risus. Sed tempor, nisi at porttitor bibendum, magna tortor malesuada libero, vitae commodo tortor mi eget sapien. Sed at dolor nec velit congue vestibulum. Cras sodales purus non ligula suscipit, nec tincidunt purus luctus. Integer viverra feugiat sollicitudin. Integer dictum egestas velit, eu bibendum purus scelerisque et. Integer eu lorem sem. Proin suscipit mauris id accumsan aliquam. Fusce suscipit arcu elit, ac laoreet augue lacinia sit amet. Nunc congue dictum volutpat. Phasellus ullamcorper dignissim neque at vestibulum.
+            </p>
+            </div>
         </div>
+
     )
 }
 
 export function Lesya(){
     return(
         <div>
-            <p>Lorem ipsum1</p>
+            <div className="header">
+                <img className="poetPic" src="/images/lesya_ukrainka_portrait_public_domain.jpg"></img>
+                <div className="title">
+                <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
+                <p>Hasellus sit amet quam id sapien malesuada dignissim nec feugiat enim. Nunc nunc enim, dictum sed ipsum ac, luctus vulputate leo. Phasellus convallis aliquet erat, eu pellentesque metus. Etiam vel molestie magna, ut porta lorem. Donec cursus elit nec maximus mattis. Cras consectetur mattis lobortis. Sed quis urna et orci vehicula volutpat. Donec pulvinar felis vel finibus lacinia. Sed sed ornare ante. 
+                    Nulla nec metus id est ullamcorper malesuada. Nullam magna nibh, lobortis sit amet porttitor eget, dictum finibus lorem. Morbi congue auctor elit, at ultricies velit varius ut. Suspendisse gravida, quam sit amet tincidunt dapibus, nunc nisi viverra velit, quis ultricies dui orci non lectus. Suspendisse non lobortis tortor. Ut id turpis ac risus venenatis elementum. Vivamus vel semper libero.
+
+                 </p></div>
+            </div>
+            <div className="general">
+                <p>
+                Phasellus sit amet quam id sapien malesuada dignissim nec feugiat enim. Nunc nunc enim, dictum sed ipsum ac, luctus vulputate leo. Phasellus convallis aliquet erat, eu pellentesque metus. Etiam vel molestie magna, ut porta lorem. Donec cursus elit nec maximus mattis. Cras consectetur mattis lobortis. Sed quis urna et orci vehicula volutpat. Donec pulvinar felis vel finibus lacinia. Sed sed ornare ante. Nulla nec metus id est ullamcorper malesuada. Nullam magna nibh, lobortis sit amet porttitor eget, dictum finibus lorem. Morbi congue auctor elit, at ultricies velit varius ut. Suspendisse gravida, quam sit amet tincidunt dapibus, nunc nisi viverra velit, quis ultricies dui orci non lectus. Suspendisse non lobortis tortor. Ut id turpis ac risus venenatis elementum. Vivamus vel semper libero.
+
+                Nam non nisi non nulla ornare feugiat quis ac nibh. Integer in efficitur nunc, vel sodales tortor. Sed pharetra mauris nisi, vitae semper felis vehicula at. Maecenas in lacus a dui congue convallis. Mauris tristique mollis tellus et efficitur. Duis consequat mi nisl, et pellentesque orci aliquam id. Vestibulum quis feugiat nisi. Mauris volutpat ultrices feugiat. Fusce placerat mi quis pretium eleifend. Morbi facilisis libero nec ornare aliquam. Suspendisse laoreet aliquam posuere. Fusce bibendum scelerisque quam, vitae congue quam tincidunt at. Nullam a neque nec felis maximus congue eu non arcu. Suspendisse euismod vel ipsum lobortis laoreet. Donec pharetra tristique eleifend.
+
+                Suspendisse potenti. Maecenas sit amet justo libero. Nullam viverra nulla ut ligula placerat ornare. Pellentesque ut ipsum odio. Donec nisl lectus, sodales quis maximus ut, rhoncus quis libero. Aliquam quis lectus quis massa lobortis aliquam. Pellentesque non justo mi. Nullam nec lacus tellus. Fusce diam quam, luctus sed ultrices a, blandit quis metus. Sed gravida vestibulum lorem, quis lacinia eros dictum in. Nullam ac tincidunt nibh. Vivamus pharetra ipsum vitae magna vestibulum, at lacinia velit aliquam.
+
+                Vivamus quis nisl velit. Pellentesque fermentum, libero ac interdum dapibus, urna eros consectetur felis, vel efficitur lacus diam imperdiet elit. Mauris consectetur sapien urna, eleifend commodo lacus elementum id. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc dictum rhoncus nisl, vitae malesuada dui maximus id. Morbi varius dignissim est sed pharetra. Aenean quis purus nec enim hendrerit convallis at ornare sapien. In ultricies eros non nulla auctor, vel viverra turpis aliquam.
+
+                Pellentesque tempus placerat ex, ac tristique velit. Cras hendrerit suscipit ipsum, et tincidunt risus. Sed tempor, nisi at porttitor bibendum, magna tortor malesuada libero, vitae commodo tortor mi eget sapien. Sed at dolor nec velit congue vestibulum. Cras sodales purus non ligula suscipit, nec tincidunt purus luctus. Integer viverra feugiat sollicitudin. Integer dictum egestas velit, eu bibendum purus scelerisque et. Integer eu lorem sem. Proin suscipit mauris id accumsan aliquam. Fusce suscipit arcu elit, ac laoreet augue lacinia sit amet. Nunc congue dictum volutpat. Phasellus ullamcorper dignissim neque at vestibulum.
+            </p>
+            </div>
         </div>
     )
 }
@@ -17,7 +60,28 @@ export function Lesya(){
 export function Franko(){
     return(
         <div>
-            <p>Lorem ipsum21</p>
+            <div className="header">
+                <img className="poetPic" src="/images/bzz1559050122FO6.jpeg"></img>
+                <div className="title">
+                <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
+                <p>Hasellus sit amet quam id sapien malesuada dignissim nec feugiat enim. Nunc nunc enim, dictum sed ipsum ac, luctus vulputate leo. Phasellus convallis aliquet erat, eu pellentesque metus. Etiam vel molestie magna, ut porta lorem. Donec cursus elit nec maximus mattis. Cras consectetur mattis lobortis. Sed quis urna et orci vehicula volutpat. Donec pulvinar felis vel finibus lacinia. Sed sed ornare ante. 
+                    Nulla nec metus id est ullamcorper malesuada. Nullam magna nibh, lobortis sit amet porttitor eget, dictum finibus lorem. Morbi congue auctor elit, at ultricies velit varius ut. Suspendisse gravida, quam sit amet tincidunt dapibus, nunc nisi viverra velit, quis ultricies dui orci non lectus. Suspendisse non lobortis tortor. Ut id turpis ac risus venenatis elementum. Vivamus vel semper libero.
+
+                 </p></div>
+            </div>
+            <div className="general">
+                <p>
+                Phasellus sit amet quam id sapien malesuada dignissim nec feugiat enim. Nunc nunc enim, dictum sed ipsum ac, luctus vulputate leo. Phasellus convallis aliquet erat, eu pellentesque metus. Etiam vel molestie magna, ut porta lorem. Donec cursus elit nec maximus mattis. Cras consectetur mattis lobortis. Sed quis urna et orci vehicula volutpat. Donec pulvinar felis vel finibus lacinia. Sed sed ornare ante. Nulla nec metus id est ullamcorper malesuada. Nullam magna nibh, lobortis sit amet porttitor eget, dictum finibus lorem. Morbi congue auctor elit, at ultricies velit varius ut. Suspendisse gravida, quam sit amet tincidunt dapibus, nunc nisi viverra velit, quis ultricies dui orci non lectus. Suspendisse non lobortis tortor. Ut id turpis ac risus venenatis elementum. Vivamus vel semper libero.
+
+                Nam non nisi non nulla ornare feugiat quis ac nibh. Integer in efficitur nunc, vel sodales tortor. Sed pharetra mauris nisi, vitae semper felis vehicula at. Maecenas in lacus a dui congue convallis. Mauris tristique mollis tellus et efficitur. Duis consequat mi nisl, et pellentesque orci aliquam id. Vestibulum quis feugiat nisi. Mauris volutpat ultrices feugiat. Fusce placerat mi quis pretium eleifend. Morbi facilisis libero nec ornare aliquam. Suspendisse laoreet aliquam posuere. Fusce bibendum scelerisque quam, vitae congue quam tincidunt at. Nullam a neque nec felis maximus congue eu non arcu. Suspendisse euismod vel ipsum lobortis laoreet. Donec pharetra tristique eleifend.
+
+                Suspendisse potenti. Maecenas sit amet justo libero. Nullam viverra nulla ut ligula placerat ornare. Pellentesque ut ipsum odio. Donec nisl lectus, sodales quis maximus ut, rhoncus quis libero. Aliquam quis lectus quis massa lobortis aliquam. Pellentesque non justo mi. Nullam nec lacus tellus. Fusce diam quam, luctus sed ultrices a, blandit quis metus. Sed gravida vestibulum lorem, quis lacinia eros dictum in. Nullam ac tincidunt nibh. Vivamus pharetra ipsum vitae magna vestibulum, at lacinia velit aliquam.
+
+                Vivamus quis nisl velit. Pellentesque fermentum, libero ac interdum dapibus, urna eros consectetur felis, vel efficitur lacus diam imperdiet elit. Mauris consectetur sapien urna, eleifend commodo lacus elementum id. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc dictum rhoncus nisl, vitae malesuada dui maximus id. Morbi varius dignissim est sed pharetra. Aenean quis purus nec enim hendrerit convallis at ornare sapien. In ultricies eros non nulla auctor, vel viverra turpis aliquam.
+
+                Pellentesque tempus placerat ex, ac tristique velit. Cras hendrerit suscipit ipsum, et tincidunt risus. Sed tempor, nisi at porttitor bibendum, magna tortor malesuada libero, vitae commodo tortor mi eget sapien. Sed at dolor nec velit congue vestibulum. Cras sodales purus non ligula suscipit, nec tincidunt purus luctus. Integer viverra feugiat sollicitudin. Integer dictum egestas velit, eu bibendum purus scelerisque et. Integer eu lorem sem. Proin suscipit mauris id accumsan aliquam. Fusce suscipit arcu elit, ac laoreet augue lacinia sit amet. Nunc congue dictum volutpat. Phasellus ullamcorper dignissim neque at vestibulum.
+            </p>
+            </div>
         </div>
     )
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,19 +1,35 @@
 import { Link, Outlet } from 'react-router-dom'
+import React, { useState } from 'react';
 import './Home.css'
 
 export function Home()
 {
+    const [value, setValue] = useState('Light');
+
+    function changeTheme(event){
+
+        let a = event.target.value;
+
+        if(a === 'Light'){
+            setValue('Light')
+        }
+        else{
+            setValue('Dark')
+        }
+    }
+
+
     return(
         <div className='wrapper'>
             <nav id='nav'>
                 <form className='forma'>
                     <div>
                         <span>Dark</span>
-                        <input className='radioBtn-theme' type="radio" name="theme"></input>
+                        <input className='radioBtn-theme' type="radio" name="theme" value='Dark' onChange={changeTheme}></input>
                     </div>
                     <div>
                         <span>Light</span>
-                        <input className='radioBtn-theme' type="radio" name="theme"></input>
+                        <input className='radioBtn-theme' type="radio" name="theme" value='Light' onChange={changeTheme} defaultChecked></input>
                     </div>
                 </form>
                 <li>
@@ -32,7 +48,7 @@ export function Home()
                     </div>
                 </li>
             </nav>
-            <div className='main'>
+            <div className={value}>
                 <Outlet></Outlet>
             </div>
             <div id='footer'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sit amet quam id sapien malesuada dignissim nec feugiat enim. Nunc nunc enim, dictum sed ipsum ac, luctus vulputate leo. Phasellus convallis aliquet erat, eu pellentesque metus. Etiam vel molestie magna, ut porta lorem. Donec cursus elit nec maximus mattis. Cras consectetur mattis lobortis. Sed quis urna et orci vehicula volutpat. Donec pulvinar felis vel finibus lacinia. Sed sed ornare ante. Nulla nec metus id est ullamcorper malesuada. Nullam magna nibh, lobortis sit amet porttitor eget, dictum finibus lorem. Morbi congue auctor elit, at ultricies velit varius ut. Suspendisse gravida, quam sit amet tincidunt dapibus, nunc nisi viverra velit, quis ultricies dui orci non lectus. Suspendisse non lobortis tortor. Ut id turpis ac risus venenatis elementum. Vivamus vel semper libero.t</div>
